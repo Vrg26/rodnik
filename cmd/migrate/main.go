95abcc326a6a4a6365f35f79a7abb72a9049732b
Ivan Gusev <gusevskif1@gmail.com>
2022-05-08 00:24:09 +0300

    Added init migrations using goose

---
+++ cmd/migrate/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"github.com/pressly/goose/v3"
+	"log"
+	"rodnik/config"
+)
+
+func main() {
+	log.Println("Start migration")
+	cfg, err := config.GetConfig()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	sqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s, dbname=%s sslmode=%s",
+		cfg.PG.Host, cfg.PG.Port, cfg.PG.User, cfg.PG.Password, cfg.PG.DBName, cfg.PG.SSLMode)
+	db, err := sql.Open("postgres", sqlInfo)
+	if err != nil {
+		log.Fatalf("Error connectiong: %v\n", err)
+	}
+	err = goose.Up(db, "./migrations")
+	if err != nil {
+		log.Fatalf("Error during migration: %v\n", err)
+	}
+	if err = db.Close(); err != nil {
+		log.Fatalf("Error close DB: %v\n", err)
+	}
+
+	log.Println("Migration completed successfully")
+}
---
+++ migrations/20220506005427_users.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+CREATE TABLE users (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    name text NOT NULL,
+    phone text NOT NULL UNIQUE,
+    password text NOT NULL,
+    balance decimal
+);
+-- +goose StatementBegin
+SELECT 'up SQL query';
+-- +goose StatementEnd
+
+-- +goose Down
+DROP TABLE users;
+-- +goose StatementBegin
+SELECT 'down SQL query';
+-- +goose StatementEnd
