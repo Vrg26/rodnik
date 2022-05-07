50ba0d861b9df48d1f63a73448bbe87b7b2f5a25
Ivan Gusev <gusevskif1@gmail.com>
2022-05-08 00:26:45 +0300

    Added postgres user repository

--- internal/entity/User.go
+++ internal/entity/User.go
@@ -3,9 +3,9 @@ package entity
 import "github.com/google/uuid"
 
 type User struct {
-	Id       uuid.UUID `json:"id" format:"uuid"`
-	Name     string    `json:"name"`
-	Phone    string    `json:"phone"`
-	Password string    `json:"password"`
-	Leaves   float64   `json:"leaves"`
+	Id       uuid.UUID `json:"id" db:"id"`
+	Name     string    `json:"name" db:"name"`
+	Phone    string    `json:"phone" db:"phone"`
+	Password string    `json:"password" db:"password"`
+	Leaves   float64   `json:"leaves" db:"balance"`
 }
