766052505d424fd4a192d2e210141607d33e98f9
Ivan Gusev <gusevskif1@gmail.com>
2022-05-09 03:24:55 +0300

    Added method FindByID in repository task_postgres

--- internal/repository/task_postgres.go
+++ internal/repository/task_postgres.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"context"
 	"github.com/Masterminds/squirrel"
+	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 	"rodnik/internal/entity"
 	"rodnik/pkg/logger"
@@ -16,6 +17,9 @@ type taskPostgres struct {
 const (
 	taskTableName = "tasks"
 )
+
+//TODO Информация есть в структуре
+var allColumnsTask = []string{"id", "title", "description", "status_id", "creator_id", "helper_id", "cost", "date_relevance", "created_on", "overdue"}
 
 func NewTaskPostgresRep(db *sqlx.DB, logger logger.Logger) *taskPostgres {
 	return &taskPostgres{db, logger}
@@ -39,4 +43,21 @@ func (r *taskPostgres) Create(ctx context.Context, task *entity.Task) (*entity.T
 		return nil, err
 	}
 	return newTask, nil
+}
+
+func (r *taskPostgres) FindByID(ctx context.Context, taskID uuid.UUID) (*entity.Task, error) {
+	task := &entity.Task{}
+
+	sqlText, values, err := squirrel.Select(allColumnsTask...).
+		From(taskTableName).
+		Where("id = $1", taskID.String()).
+		ToSql()
+
+	if err != nil {
+		return nil, err
+	}
+	if err = r.db.GetContext(ctx, task, sqlText, values...); err != nil {
+		return nil, err
+	}
+	return task, nil
 }
